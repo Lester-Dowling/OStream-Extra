7aea5d40a1cee0a683b878e956810a01ed18081f
Lester Dowling <Lester.Dowling@gmail.com>
2019-09-08 03:04:28 +1000

    Extract all source files from another project; unit tests run successfully.

---
+++ Unit-Tests/pch-unit-tests.hpp
@@ -0,0 +1,72 @@
+﻿#pragma once
+#ifdef _WINDOWS
+#include <WinSDKVer.h>
+#include <SDKDDKVer.h>
+// include <windows.h>
+#endif // _WINDOWS
+#include <cassert>
+#include <cstdlib>
+
+#include <algorithm>
+#include <initializer_list>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <iterator>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <deque>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <vector>
+#include <stdexcept>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/find.hpp>
+#include <boost/algorithm/string/find_iterator.hpp>
+#include <boost/algorithm/string/regex.hpp>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/bind.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
+#include <boost/system/system_error.hpp>
+#include <boost/tokenizer.hpp>
+
+namespace std {
+	template<typename T>
+	ostream& operator<<(ostream& o, const std::stack<T>& s)
+	{
+		std::stack<T> y{ s };
+		bool first_e = true;
+		while (!y.empty()) {
+			const auto e = y.top();
+			y.pop();
+			if (first_e) {
+				o << e;
+				first_e = false;
+			}
+			o << ',' << ' ' << e;
+		}
+		return o;
+	}
+
+	template<typename T>
+	ostream& operator<<(ostream& o, const std::deque<T>& s)
+	{
+		bool first_e = true;
+		for (const auto& e : s) {
+			if (first_e) {
+				o << e;
+				first_e = false;
+			}
+			o << ',' << ' ' << e;
+		}
+		return o;
+	}
+} // namespace std
